8e73425db0587f4305620a5d8b03e9d93e7f1a70
Cyber Knight <cyberknight755@gmail.com>
2023-02-26 04:58:12 +0800

    DisplayFeatures: Import DisplayFeaturesHbmTileService
    - Imported and adapted from KProfiles Frontend.
    - Incorporate minimality from [1].
    
    [1]: https://github.com/Agatha-Prjkt/device_xiaomi_spes/blob/9489f4d27db62e3bbfce339e5f394c90818d0ec9/parts/src/org/lineageos/settings/display/DcDimmingSettingsFragment.java
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ src/com/android/displayfeatures/display/DisplayFeaturesHbmTileService.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023 cyberknight777
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.displayfeatures.display;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.preference.PreferenceManager;
+
+import com.android.displayfeatures.R;
+import com.android.displayfeatures.utils.FileUtils;
+
+public class DisplayFeaturesHbmTileService extends TileService {
+
+    private static final Context mContext;
+    private SharedPreferences mSharedPrefs;
+
+    private static final String DISPLAYFEATURES_HBM_KEY = "hbm";
+    private static final String DISPLAYFEATURES_HBM_NODE = mContext.getResources().getString(com.android.displayfeatures.R.string.config_DisplayFeaturesHbmPath);
+
+    private void updateUI(boolean enabled) {
+        final Tile tile = getQsTile();
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        updateUI(sharedPrefs.getBoolean(DISPLAYFEATURES_HBM_KEY, false));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean enabled = !(sharedPrefs.getBoolean(DISPLAYFEATURES_HBM_KEY, false));
+        FileUtils.writeLine(DISPLAYFEATURES_HBM_NODE, enabled ? "1" : "0");
+        sharedPrefs.edit().putBoolean(DISPLAYFEATURES_HBM_KEY, enabled).commit();
+        updateUI(enabled);
+    }
+}
