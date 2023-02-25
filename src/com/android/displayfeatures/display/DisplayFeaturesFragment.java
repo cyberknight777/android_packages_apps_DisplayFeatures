21c761fd5d1654cc0a873d481003f7d11f295d79
Cyber Knight <cyberknight755@gmail.com>
2023-02-26 04:41:38 +0800

    DisplayFeatures: Import DisplayFeaturesFragment
    - Imported and adapted from [1].
    - Add more whitespace between variable definitions.
    
    [1]: https://github.com/CannedShroud/device_xiaomi_raphael/blob/6a7c5ef65c1b24c133a24a21a81ff341d5dee956/parts/src/org/lineageos/settings/display/DisplaySettingsFragment.java
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ src/com/android/displayfeatures/display/DisplayFeaturesFragment.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2020 YAAP
+ * Copyright (C) 2023 cyberknight777
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.displayfeatures.display;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.displayfeatures.R;
+import com.android.displayfeatures.utils.FileUtils;
+
+public class DisplayFeaturesFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final Context mContext;
+    private SwitchPreference mDcDimmingPreference;
+    private static final String DISPLAYFEATURES_DC_DIMMING_KEY = "dc_dimming";
+    private static final String DISPLAYFEATURES_DC_DIMMING_NODE = mContext.getResources().getString(com.android.displayfeatures.R.string.config_DisplayFeaturesDcDimPath);
+
+    private SwitchPreference mHBMPreference;
+    private static final String DISPLAYFEATURES_HBM_KEY = "hbm";
+    private static final String DISPLAYFEATURES_HBM_NODE = mContext.getResources().getString(com.android.displayfeatures.R.string.config_DisplayFeaturesHbmPath);
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.displayfeatures_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        mDcDimmingPreference = (SwitchPreference) findPreference(DISPLAYFEATURES_DC_DIMMING_KEY);
+        if (FileUtils.fileExists(DISPLAYFEATURES_DC_DIMMING_NODE)) {
+            mDcDimmingPreference.setEnabled(true);
+            mDcDimmingPreference.setOnPreferenceChangeListener(this);
+        } else {
+            mDcDimmingPreference.setSummary(R.string.dc_dimming_summary_not_supported);
+            mDcDimmingPreference.setEnabled(false);
+        }
+        mHBMPreference = (SwitchPreference) findPreference(DISPLAYFEATURES_HBM_KEY);
+        if (FileUtils.fileExists(DISPLAYFEATURES_HBM_NODE)) {
+            mHBMPreference.setEnabled(true);
+            mHBMPreference.setOnPreferenceChangeListener(this);
+        } else {
+            mHBMPreference.setSummary(R.string.hbm_summary_not_supported);
+            mHBMPreference.setEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (DISPLAYFEATURES_DC_DIMMING_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(DISPLAYFEATURES_DC_DIMMING_NODE, (Boolean) newValue ? "1":"0");
+        }
+        if (DISPLAYFEATURES_HBM_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(DISPLAYFEATURES_HBM_NODE, (Boolean) newValue ? "1" : "0");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+}
