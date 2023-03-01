cyberknight777/android_packages_apps_DisplayFeatures

commit ee376d6eb06917f59b0af18520e0c48bfa3f943f

DisplayFeatures: Rewrite key/node check
- Create a simpleton class to organize common variables accordingly.
- This should reduce I/O usage and micro-optimize the checks.
- Heavily inspired by [1].

[1]: https://github.com/yaap/packages_apps_OpenDelta/blob/thirteen/src/eu/chainfire/opendelta/Config.java

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

src/com/android/displayfeatures/display/DisplayFeaturesHbmTileService.java
@@ -16,8 +16,6 @@
 
 package com.android.displayfeatures.display;
 
-import android.content.res.Resources;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
@@ -29,9 +27,7 @@ import com.android.displayfeatures.utils.FileUtils;
 
 public class DisplayFeaturesHbmTileService extends TileService {
 
-    private Context mContext;
-    private static final String DISPLAYFEATURES_HBM_KEY = "hbm";
-    private String DISPLAYFEATURES_HBM_NODE;
+    private DisplayFeaturesConfig mConfig;
 
     private void updateUI(boolean enabled) {
         final Tile tile = getQsTile();
@@ -42,8 +38,9 @@ public class DisplayFeaturesHbmTileService extends TileService {
     @Override
     public void onStartListening() {
         super.onStartListening();
+        mConfig = DisplayFeaturesConfig.getInstance(this);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        updateUI(sharedPrefs.getBoolean(DISPLAYFEATURES_HBM_KEY, false));
+        updateUI(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_HBM_KEY, false));
     }
 
     @Override
@@ -55,14 +52,11 @@ public class DisplayFeaturesHbmTileService extends TileService {
     public void onClick() {
         super.onClick();
 
-        mContext = this;
-        Resources res = mContext.getResources();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        DISPLAYFEATURES_HBM_NODE = res.getString(R.string.config_DisplayFeaturesHbmPath);
-        final boolean enabled = !(sharedPrefs.getBoolean(DISPLAYFEATURES_HBM_KEY, false));
+        final boolean enabled = !(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false));
 
-        FileUtils.writeLine(DISPLAYFEATURES_HBM_NODE, enabled ? "1" : "0");
-        sharedPrefs.edit().putBoolean(DISPLAYFEATURES_HBM_KEY, enabled).commit();
+        FileUtils.writeLine(mConfig.getHbmPath(), enabled ? "1" : "0");
+        sharedPrefs.edit().putBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, enabled).commit();
         updateUI(enabled);
     }
 }

src/com/android/displayfeatures/BootCompletedReceiver.java
@@ -21,8 +21,8 @@ package com.android.displayfeatures;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.util.Log;
+import com.android.displayfeatures.display.DisplayFeaturesConfig;
 import com.android.displayfeatures.utils.FileUtils;
 import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
@@ -31,26 +31,19 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "DisplayFeatures";
 
-    private static final String DISPLAYFEATURES_DC_DIMMING_KEY = "dc_dimming";
-    private String DISPLAYFEATURES_DC_DIMMING_NODE;
-
-    private static final String DISPLAYFEATURES_HBM_KEY = "hbm";
-    private String DISPLAYFEATURES_HBM_NODE;
-
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
 
-        Resources res = context.getResources();
-        DISPLAYFEATURES_DC_DIMMING_NODE = res.getString(R.string.config_DisplayFeaturesDcDimPath);
-        DISPLAYFEATURES_HBM_NODE = res.getString(R.string.config_DisplayFeaturesHbmPath);
+        DisplayFeaturesConfig mConfig = DisplayFeaturesConfig.getInstance(context);
+
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DISPLAYFEATURES_DC_DIMMING_KEY, false);
-        boolean hbmEnabled = sharedPrefs.getBoolean(DISPLAYFEATURES_HBM_KEY, false);
-        FileUtils.writeLine(DISPLAYFEATURES_DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
-        FileUtils.writeLine(DISPLAYFEATURES_HBM_NODE, hbmEnabled ? "1" : "0");
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false);
+        boolean hbmEnabled = sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_HBM_KEY, false);
+        FileUtils.writeLine(mConfig.getDcDimPath(), dcDimmingEnabled ? "1" : "0");
+        FileUtils.writeLine(mConfig.getHbmPath(), hbmEnabled ? "1" : "0");
 
     }
 }

src/com/android/displayfeatures/display/DisplayFeaturesConfig.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2023 cyberknight777
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.displayfeatures.display;
+
+import android.content.Context;
+import android.content.res.Resources;
+import com.android.settingslib.widget.R;
+
+public class DisplayFeaturesConfig {
+
+    private static DisplayFeaturesConfig instance = null;
+
+    public static DisplayFeaturesConfig getInstance(Context context) {
+
+        if (instance == null) {
+            instance = new DisplayFeaturesConfig(context.getApplicationContext());
+        }
+
+        return instance;
+    }
+
+    public static final String DISPLAYFEATURES_DC_DIMMING_KEY = "dc_dimming";
+    public static final String DISPLAYFEATURES_HBM_KEY = "hbm";
+
+    private final String config_DisplayFeaturesDcDimPath;
+    private final String config_DisplayFeaturesHbmPath;
+
+    private DisplayFeaturesConfig(Context context) {
+
+	Resources res = context.getResources();
+
+	config_DisplayFeaturesDcDimPath = res.getString(R.string.config_DisplayFeaturesDcDimPath);
+	config_DisplayFeaturesHbmPath = res.getString(R.string.config_DisplayFeaturesHbmPath);
+
+    }
+
+     public String getDcDimPath() {
+        return config_DisplayFeaturesDcDimPath;
+    }
+
+     public String getHbmPath() {
+        return config_DisplayFeaturesHbmPath;
+    }
+ }

src/com/android/displayfeatures/display/DisplayFeaturesDcDimTileService.java
@@ -16,8 +16,6 @@
 
 package com.android.displayfeatures.display;
 
-import android.content.res.Resources;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
@@ -30,9 +28,7 @@ import com.android.displayfeatures.utils.FileUtils;
 
 public class DisplayFeaturesDcDimTileService extends TileService {
 
-    private Context mContext;
-    private static final String DISPLAYFEATURES_DC_DIMMING_KEY = "dc_dimming";
-    private String DISPLAYFEATURES_DC_DIMMING_NODE;
+    private DisplayFeaturesConfig mConfig;
 
     private void updateUI(boolean enabled) {
         final Tile tile = getQsTile();
@@ -44,8 +40,9 @@ public class DisplayFeaturesDcDimTileService extends TileService {
     @Override
     public void onStartListening() {
         super.onStartListening();
+        mConfig = DisplayFeaturesConfig.getInstance(this);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        updateUI(sharedPrefs.getBoolean(DISPLAYFEATURES_DC_DIMMING_KEY, false));
+        updateUI(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false));
     }
 
     @Override
@@ -57,14 +54,11 @@ public class DisplayFeaturesDcDimTileService extends TileService {
     public void onClick() {
         super.onClick();
 
-        mContext = this;
-        Resources res = mContext.getResources();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        DISPLAYFEATURES_DC_DIMMING_NODE = res.getString(R.string.config_DisplayFeaturesDcDimPath);
-        final boolean enabled = !(sharedPrefs.getBoolean(DISPLAYFEATURES_DC_DIMMING_KEY, false));
+        final boolean enabled = !(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false));
 
-        FileUtils.writeLine(DISPLAYFEATURES_DC_DIMMING_NODE, enabled ? "1" : "0");
-        sharedPrefs.edit().putBoolean(DISPLAYFEATURES_DC_DIMMING_KEY, enabled).commit();
+        FileUtils.writeLine(mConfig.getDcDimPath(), enabled ? "1" : "0");
+        sharedPrefs.edit().putBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, enabled).commit();
         updateUI(enabled);
     }
 }

src/com/android/displayfeatures/display/DisplayFeaturesFragment.java
@@ -17,8 +17,6 @@
 
 package com.android.displayfeatures.display;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -31,34 +29,23 @@ public class DisplayFeaturesFragment extends PreferenceFragmentCompat implements
         Preference.OnPreferenceChangeListener {
 
     private SwitchPreference mDcDimmingPreference;
-    private static final String DISPLAYFEATURES_DC_DIMMING_KEY = "dc_dimming";
-    private String DISPLAYFEATURES_DC_DIMMING_NODE;
-
     private SwitchPreference mHBMPreference;
-    private static final String DISPLAYFEATURES_HBM_KEY = "hbm";
-    private String DISPLAYFEATURES_HBM_NODE;
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Resources res = context.getResources();
-        DISPLAYFEATURES_DC_DIMMING_NODE = res.getString(R.string.config_DisplayFeaturesDcDimPath);
-        DISPLAYFEATURES_HBM_NODE = res.getString(R.string.config_DisplayFeaturesHbmPath);
-    }
+    private DisplayFeaturesConfig mConfig;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.displayfeatures_settings, rootKey);
-        mDcDimmingPreference = (SwitchPreference) findPreference(DISPLAYFEATURES_DC_DIMMING_KEY);
-        if (FileUtils.fileExists(DISPLAYFEATURES_DC_DIMMING_NODE)) {
+        mConfig = DisplayFeaturesConfig.getInstance(getContext());
+        mDcDimmingPreference = (SwitchPreference) findPreference(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY);
+        if (FileUtils.fileExists(mConfig.getDcDimPath())) {
             mDcDimmingPreference.setEnabled(true);
             mDcDimmingPreference.setOnPreferenceChangeListener(this);
         } else {
             mDcDimmingPreference.setSummary(R.string.dc_dimming_summary_not_supported);
             mDcDimmingPreference.setEnabled(false);
         }
-        mHBMPreference = (SwitchPreference) findPreference(DISPLAYFEATURES_HBM_KEY);
-        if (FileUtils.fileExists(DISPLAYFEATURES_HBM_NODE)) {
+        mHBMPreference = (SwitchPreference) findPreference(mConfig.DISPLAYFEATURES_HBM_KEY);
+        if (FileUtils.fileExists(mConfig.getHbmPath())) {
             mHBMPreference.setEnabled(true);
             mHBMPreference.setOnPreferenceChangeListener(this);
         } else {
@@ -69,11 +56,11 @@ public class DisplayFeaturesFragment extends PreferenceFragmentCompat implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (DISPLAYFEATURES_DC_DIMMING_KEY.equals(preference.getKey())) {
-            FileUtils.writeLine(DISPLAYFEATURES_DC_DIMMING_NODE, (Boolean) newValue ? "1":"0");
+        if (mConfig.DISPLAYFEATURES_DC_DIMMING_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(mConfig.getDcDimPath(), (Boolean) newValue ? "1":"0");
         }
-        if (DISPLAYFEATURES_HBM_KEY.equals(preference.getKey())) {
-            FileUtils.writeLine(DISPLAYFEATURES_HBM_NODE, (Boolean) newValue ? "1" : "0");
+        if (mConfig.DISPLAYFEATURES_HBM_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(mConfig.getHbmPath(), (Boolean) newValue ? "1" : "0");
         }
         return true;
     }

commit 57c6dbd3daffceeb6ce16529867bf450ad95a683

DisplayFeatures: Fix tile mismatch
- This makes the tiles report values accordingly.

Fixes: ee376d6 {"DisplayFeatures: Rewrite key/node check"}
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

src/com/android/displayfeatures/display/DisplayFeaturesHbmTileService.java
@@ -53,10 +53,10 @@ public class DisplayFeaturesHbmTileService extends TileService {
         super.onClick();
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        final boolean enabled = !(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false));
+        final boolean enabled = !(sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_HBM_KEY, false));
 
         FileUtils.writeLine(mConfig.getHbmPath(), enabled ? "1" : "0");
-        sharedPrefs.edit().putBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, enabled).commit();
+        sharedPrefs.edit().putBoolean(mConfig.DISPLAYFEATURES_HBM_KEY, enabled).commit();
         updateUI(enabled);
     }
 }