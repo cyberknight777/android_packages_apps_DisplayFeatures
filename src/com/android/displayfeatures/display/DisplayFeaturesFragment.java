9a7ba2db2b1420e0afa668980c00fc43058fa2d4
Cyber Knight <cyberknight755@gmail.com>
2025-07-21 03:27:11 +0800

    DisplayFeatures: the ) goes to
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/displayfeatures/display/DisplayFeaturesFragment.java
+++ src/com/android/displayfeatures/display/DisplayFeaturesFragment.java
@@ -101,7 +101,7 @@ public class DisplayFeaturesFragment extends PreferenceFragmentCompat implements
                 final boolean cabcStarted = intent.getBooleanExtra(
                             mConfig.EXTRA_CABC_STATE, false);
 
-                mCABCPreference.setValue(mConfig.isCabcCurrentlyEnabled(mConfig.getCabcPath())));
+                mCABCPreference.setValue(mConfig.isCabcCurrentlyEnabled(mConfig.getCabcPath()));
                 mCABCPreference.setSummary(mCABCPreference.getEntry());
 
             }
