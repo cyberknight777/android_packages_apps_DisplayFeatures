cdab40aaa63b4d732e971544a392a525da11a61a
Cyber Knight <cyberknight755@gmail.com>
2025-07-21 20:13:43 +0800

    DisplayFeatures: Completely hide unavailable QS tiles
    - To maintain consistency.
    - Inspired by [1].
    
    [1]: https://github.com/yaap/device_oneplus_sm8150-common/commit/30e081da3d551aa22e9a42bcffc5a99c3f01ca80
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/displayfeatures/BootCompletedReceiver.java
+++ src/com/android/displayfeatures/BootCompletedReceiver.java
@@ -18,18 +18,29 @@
 
 package com.android.displayfeatures;
 
+import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import com.android.displayfeatures.display.DisplayFeaturesConfig;
 import com.android.displayfeatures.utils.FileUtils;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import androidx.preference.PreferenceManager;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "DisplayFeatures";
+    private static final String PKG_NAME = "com.android.displayfeatures";
+    private static final String PKG_NAME_SUFFIX = PKG_NAME + ".display";
+    private static final String DC_DIM_TILE_CLASS_NAME = PKG_NAME_SUFFIX + ".DisplayFeaturesDcDimTileService";
+    private static final String HBM_TILE_CLASS_NAME = PKG_NAME_SUFFIX + ".DisplayFeaturesHbmTileService";
+    private static final String FPS_TILE_CLASS_NAME = PKG_NAME_SUFFIX + ".DisplayFeaturesFpsTileService";
+    private static final String CABC_TILE_CLASS_NAME = PKG_NAME_SUFFIX + ".DisplayFeaturesCabcTileService";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -38,20 +49,46 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         DisplayFeaturesConfig mConfig = DisplayFeaturesConfig.getInstance(context);
 
+        PackageManager pm = context.getPackageManager();
+        ComponentName cn;
+        int state;
+
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
+        cn = new ComponentName(PKG_NAME, DC_DIM_TILE_CLASS_NAME);
         if (FileUtils.fileExists(mConfig.getDcDimPath())) {
             boolean dcDimmingEnabled = sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_DC_DIMMING_KEY, false);
             FileUtils.writeLine(mConfig.getDcDimPath(), dcDimmingEnabled ? "1" : "0");
+            state = COMPONENT_ENABLED_STATE_ENABLED;
+        } else {
+            state = COMPONENT_ENABLED_STATE_DISABLED;
         }
+        pm.setComponentEnabledSetting(cn, state, 0);
 
+        cn = new ComponentName(PKG_NAME, HBM_TILE_CLASS_NAME);
         if (FileUtils.fileExists(mConfig.getHbmPath())) {
             boolean hbmEnabled = sharedPrefs.getBoolean(mConfig.DISPLAYFEATURES_HBM_KEY, false);
             FileUtils.writeLine(mConfig.getHbmPath(), hbmEnabled ? "1" : "0");
+            state = COMPONENT_ENABLED_STATE_ENABLED;
+        } else {
+            state = COMPONENT_ENABLED_STATE_DISABLED;
         }
+        pm.setComponentEnabledSetting(cn, state, 0);
 
-        // reset prefs that reflect a state that does not retain a reboot
-        if (FileUtils.fileExists(mConfig.getFpsPath())) sharedPrefs.edit().remove(mConfig.PREF_KEY_FPS_STATE).commit();
+        cn = new ComponentName(PKG_NAME, FPS_TILE_CLASS_NAME);
+        if (FileUtils.fileExists(mConfig.getFpsPath())) {
+            // reset prefs that reflect a state that does not retain a reboot
+            sharedPrefs.edit().remove(mConfig.PREF_KEY_FPS_STATE).commit();
+            state = COMPONENT_ENABLED_STATE_ENABLED;
+        } else {
+            state = COMPONENT_ENABLED_STATE_DISABLED;
+        }
+        pm.setComponentEnabledSetting(cn, state, 0);
+
+        cn = new ComponentName(PKG_NAME, CABC_TILE_CLASS_NAME);
+        state = FileUtils.fileExists(mConfig.getCabcPath())
+                ? COMPONENT_ENABLED_STATE_ENABLED : COMPONENT_ENABLED_STATE_DISABLED;
+        pm.setComponentEnabledSetting(cn, state, 0);
 
     }
 }
